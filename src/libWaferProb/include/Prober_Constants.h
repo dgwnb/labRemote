04a84ea3fabb0fe0246c5053fdc081792976575e
Adam Molnar <amolnar@goosevulture.dhcp.lbl.gov>
2019-07-10 11:51:41 -0700

    Added new prober_constants a global constants header file

---
+++ src/libWaferProb/include/Prober_Constants.h
@@ -0,0 +1,12 @@
+#ifndef _Prober_Constants_H_
+#define _Prober_Constants_H_
+
+const int safety_limit{70000};
+const int prober_limit{75765};
+const float current_limit{0.005};
+const float test_voltage{0.5};
+
+
+
+#endif
+
